9920213b2c25e00460710449204487b1be839df0
Peter Kraska <32576481+pkraska@users.noreply.github.com>
2022-11-03 21:35:40 -0300

    updated the map setup files

--- R/ampBasemap.R
+++ R/ampBasemap.R
@@ -1,52 +1,96 @@
-ampBasemap <- function (data, coastline, aoi_buffer = 1000){
-  AOI <- data[['si']] %>%
-    sf::st_buffer(dist = 1000) %>%
-    sf::st_bbox()
+#' AMP Basemap creation for use in plotting
+#'
+#' @param data list of AMP data from ampDataLoad function
+#' @param coastline sf object from ampMapSetup function
+#' @param aoi_buffer distance in meters for how far from sample locations to
+#'   buffer for map creation
+#' @import dplyr
+#' @import sf
+#' @import ggplot2
+#' @import rnaturalearth
+#' @import cowplot
+#' @name %>%
+#'
+#'
+#' @return ggplot2 plot of sf objects
+#' @export
+#'
+#' @examples ampBasemap()
+ampBasemap <-
+  function (data,
+            coastline,
+            aoi_buffer = 1000,
+            insetMap = FALSE,
+            facility = NA,
+            cageExtents = FALSE) {
 
-  AOI_coastline <- coastline %>%
-    sf::st_crop(AOI)
+    if (is.character(facility)) {
+      data[['si']] <- data[['si']] %>%
+        filter(facilityNumberInProject %in% facility)
+    }
 
-  insetMap_bbox <- data[['si']] %>%
-    sf::st_buffer(dist = 1000) %>%
-    sf::st_bbox() %>%
-    sf::st_as_sfc(crs = 32620)
+    AOI <- data[['si']] %>%
+      sf::st_buffer(dist = 1000) %>%
+      sf::st_bbox()
 
-  insetMap_coastline <-
-    rnaturalearth::ne_countries(
-      returnclass = 'sf',
-      scale = 10,
-      country = c('Canada', 'United States of America')
-    ) %>%
-    # filter(abbrev %in% c('Can.', 'U.S.A.')) %>%
-    sf::st_transform(crs = 32620) %>%
-    sf::st_crop(sf::st_as_sfc(sf::st_bbox(sf::st_buffer(data[['si']], 50000))))
+    AOI_coastline <- coastline %>%
+      sf::st_crop(AOI)
 
-  insetMap_border <-
-    sf::st_as_sfc(sf::st_bbox(sf::st_buffer(data[['si']], 50000)))
+    insetMap_bbox <- data[['si']] %>%
+      sf::st_buffer(dist = 1000) %>%
+      sf::st_bbox() %>%
+      sf::st_as_sfc(crs = 32620)
 
-  insetMap <- ggplot() +
-    geom_sf(data = insetMap_border, fill = 'white', col = 'grey30') +
-    geom_sf(data = insetMap_coastline) +
-    geom_sf(data = insetMap_bbox, col = 'red', fill = 'red') +
-    coord_sf(expand = FALSE) +
-    theme_void()
+    if (insetMap == TRUE) {
+      insetMap_coastline <-
+        rnaturalearth::ne_countries(
+          returnclass = 'sf',
+          scale = 10,
+          country = c('Canada', 'United States of America')
+        ) %>%
+        # filter(abbrev %in% c('Can.', 'U.S.A.')) %>%
+        sf::st_transform(crs = 32620) %>%
+        sf::st_crop(sf::st_as_sfc(sf::st_bbox(sf::st_buffer(data[['si']], 50000))))
 
-  baseMap <- ggplot() +
-    geom_sf(data = insetMap_bbox, fill = NA) +
-    geom_sf(data = AOI_coastline) +
-    geom_sf(data = data[['si']]) +
-    coord_sf(expand = FALSE) +
-    theme_minimal()
-  # +
+      insetMap_border <-
+        sf::st_as_sfc(sf::st_bbox(sf::st_buffer(data[['si']], 50000)))
+
+      insetMap <- ggplot2::ggplot() +
+        ggplot2::geom_sf(data = insetMap_border,
+                         fill = 'white',
+                         col = 'grey30') +
+        ggplot2::geom_sf(data = insetMap_coastline) +
+        ggplot2::geom_sf(data = insetMap_bbox,
+                         col = 'red',
+                         fill = 'red') +
+        ggplot2::coord_sf(expand = FALSE) +
+        ggplot2::theme_void()
+    }
+    baseMap <- ggplot2::ggplot() +
+      ggplot2::geom_sf(data = insetMap_bbox, fill = NA) +
+      ggplot2::geom_sf(data = AOI_coastline, col = 'grey', fill = 'grey') +
+      ggplot2::geom_sf(data = data[['si']], aes(col = facilityNumber)) +
+      ggplot2::coord_sf(expand = FALSE) +
+      ggplot2::theme_bw()
+
+    if (cageExtents != FALSE){
+      baseMap +
+        ggplot2::geom_sf(data = cageExtents, col = 'black')
+    }
+    # +
     # labs(subtitle = paste0(facilityNum, " - ", unique(data[[facilityNum]][['si']]$facilityName)))
-
-  ggdraw() +
-    cowplot::draw_plot(baseMap) +
-    cowplot::draw_plot(
-      insetMap,
-      x = 0.74,
-      y = 0.66,
-      width = 0.25,
-      height = 0.25
-    )
-}
+    if (insetMap == TRUE) {
+      cowplot::ggdraw() +
+        cowplot::draw_plot(baseMap) +
+        cowplot::draw_plot(
+          insetMap,
+          x = 0.74,
+          y = 0.66,
+          width = 0.25,
+          height = 0.25
+        )
+    }
+    if (insetMap == FALSE){
+      return(baseMap)
+    }
+  }
